033393493f4fcb8d17b4fb65d02d3adff0bb1b94
dindakusuma57 <dindakusuma@apps.ipb.ac.id>
2024-10-07 14:57:56 +0700

    Add Pesan resource

---
+++ app/Filament/Resources/PesanResource.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PesanResource\Pages;
+use App\Filament\Resources\PesanResource\RelationManagers;
+use App\Models\Pesan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\MarkdownEditor;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PesanResource extends Resource
+{
+    protected static ?string $model = Pesan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Section::make('Footer Information')->schema([
+
+                TextInput::make('first_name')
+                        ->required()
+                        ->maxLength(255),
+
+                TextInput::make('last_name')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('email')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('phone')
+                    ->label('Phone')
+                    ->required()
+                    ->tel()
+                    ->maxLength(15)
+                    ->placeholder('+628123456789')
+                    ->rule('regex:/^[0-9+]+$/'),
+
+                TextInput::make('judul')
+                    ->required()
+                    ->maxLength(255),
+
+                MarkdownEditor::make('message')
+                    ->columnSpanFull()
+                    ->fileAttachmentsDirectory('message'),
+        ])
+    ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('judul')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('message')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPesans::route('/'),
+            'create' => Pages\CreatePesan::route('/create'),
+            'edit' => Pages\EditPesan::route('/{record}/edit'),
+        ];
+    }
+}
