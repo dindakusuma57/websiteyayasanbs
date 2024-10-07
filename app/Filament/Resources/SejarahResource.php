f9ffa3e64ffa26afce3e2a186c9c9a936011371b
dindakusuma57 <dindakusuma@apps.ipb.ac.id>
2024-10-07 13:14:05 +0700

    Add sejarah resource

---
+++ app/Filament/Resources/SejarahResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SejarahResource\Pages;
+use App\Filament\Resources\SejarahResource\RelationManagers;
+use App\Models\Sejarah;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SejarahResource extends Resource
+{
+    protected static ?string $model = Sejarah::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Section::make([
+                Grid::make()
+                    ->schema([
+                        TextInput::make('judul')
+                            ->required()
+                            ->maxLength(255),
+                        MarkdownEditor::make('deskripsi')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('sejarah'),
+                    ])
+            ])
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('judul')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('deskripsi')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make()
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSejarahs::route('/'),
+            'create' => Pages\CreateSejarah::route('/create'),
+            'edit' => Pages\EditSejarah::route('/{record}/edit'),
+        ];
+    }
+}
